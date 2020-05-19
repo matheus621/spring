bc9b3ef106845e69392aeeb5431dd3528d6aadd1
matheus621 <matheus621cardoso@gmail.com>
2020-05-18 21:17:52 -0300

    Initial commit

---
+++ src/test/java/com/souza/springcourse/SpringCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.souza.springcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

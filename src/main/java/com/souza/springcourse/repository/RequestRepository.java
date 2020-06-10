6b62e223c86e59b77decb194f0233a2e4e8244dd
matheus621 <matheus621cardoso@gmail.com>
2020-06-10 13:14:42 -0300

    update project

--- src/main/java/com/souza/springcourse/repository/RequestRepository.java
+++ src/main/java/com/souza/springcourse/repository/RequestRepository.java
@@ -1,11 +1,20 @@
 package com.souza.springcourse.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.souza.springcourse.domain.Request;
+import com.souza.springcourse.domain.enums.RequestState;
 
 @Repository
 public interface RequestRepository extends JpaRepository<Request, Long>{
 
+	
+	public List<Request> findAllByOwnerId(Long id);
+	
+	@Query("UPDATE Request SET state = ?2 WHERE id = ?1")
+	public Request updateStatus(Long id, RequestState state);
 }
